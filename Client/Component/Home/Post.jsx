840577d435e276ccc8b401d81543c44e92c43078
Denyel <denyeljeff@gmail.com>
2020-04-15 18:00:49 -0300

    Example meteor react stuff

---
+++ Client/Component/Home/Post.jsx
@@ -0,0 +1,12 @@
+Post = React.createClass({
+    render(){
+        let {post} = this.props;
+
+        return (
+            <div className="card">
+                <h3>{post.user}</h3>
+                <p>{post.content}</p>
+            </div>
+        )
+    }
+});
---
+++ lib/Router.jsx
@@ -0,0 +1,23 @@
+FlowRouter.route("/", {
+    name: "Home",
+    action(params){
+        renderView(<Home />);
+    }
+});
+
+FlowRouter.route("/posts-by/:user",{
+    name: "Posts",
+    action(params){
+        console.log(params.user);
+        renderView(<Home user={params.user} />);
+    }
+});
+
+function renderView(component){
+    ReactLayout.render(MainLayout, {
+        header: <Header />,
+        content: component,
+        footer: <Footer />,
+        logThis: "This is a property"
+    });
+}
